2ead79096e186ecee23b63cdabe18dcda7d1a2f4
TankorSmash <tankorsmash@gmail.com>
2013-01-06 12:10:44 -0500

    Added the files from the last commit

---
+++ NextFlicksMVC4/Tracking/Tracking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using NextFlicksMVC4.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NextFlicksMVC4.Tracking
+{
+
+    public class UserLog
+    {
+        [Key]
+        public int userlog_id { get; set; }
+
+        [DisplayName("IP Address")]
+        public string ip_addr { get; set; }
+
+        [DisplayName("Time")]
+        public DateTime time { get; set; }
+
+        [DisplayName("User Agent")]
+        public string useragent { get; set; }
+
+        [DisplayName("Target Raw URL")]
+        public string raw_url { get; set; }
+
+        [DisplayName("Referrer")]
+        public string referrer { get; set; }
+
+        //probably need to track cookies or something here too
+
+    }
+
+
+    public class TrackingDbContext : DbContext
+    {
+        public DbSet<UserLog> UserLogs { get; set; }
+    }
+}
