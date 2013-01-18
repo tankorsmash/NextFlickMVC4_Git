fe4c6ec6e1f3aabb03a731a79e1dbad2dc731a0a
sec_goat <joshua.tilson@yahoo.com>
2013-01-18 13:27:31 -0500

    Forgot to add a couple of files.

---
+++ NextFlicksMVC4/Views/Movies/ViewModels/MovieTagViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using NextFlicksMVC4.Models;
+
+namespace NextFlicksMVC4.Views.Movies.ViewModels
+{
+    public class MovieTagViewModel
+    {
+        public Movie movie { get; set; }
+
+        [DisplayName("List of Genres")]
+        [DataType("CommaList")]
+        public List<string> genre_strings { get; set; }
+
+        public BoxArt Boxart { get; set; }
+
+        [DisplayName("Tags")]
+        [DataType("CommaList")]
+        public List<String> Tags { get; set; }
+    }
+}
