cf27910089d855cc6be15cfc05b82f2b1c402c9a
sec_goat <joshua.tilson@yahoo.com>
2013-01-27 12:15:33 -0500

    added some missing files

---
+++ NextFlicksMVC4/Models/TagModels/TagDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NextFlicksMVC4.Models.userAccount;
+
+namespace NextFlicksMVC4.Models.TagModels
+{
+    public class TagDetailViewModel
+    {
+        public String TagName { get; set; }
+        public List<Movie> TaggedMovies { get; set; }
+        public Dictionary<Users, int> TaggedByUsers { get; set; } 
+
+    }
+}
---
+++ NextFlicksMVC4/Models/TagModels/TagViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using NextFlicksMVC4.Models;
+using NextFlicksMVC4.Models.userAccount;
+
+namespace NextFlicksMVC4.Models.TagModels
+{
+    public class TagCountViewModel
+    {
+      /*  public Movie movie { get; set; }
+
+        [DisplayName("List of Genres")]
+        [DataType("CommaList")]
+        public List<string> genre_strings { get; set; }
+
+        public BoxArt Boxart { get; set; }
+
+        /* don't need ths one any more as it was a sad once trick pony that didnt do wnat i needed
+        [DisplayName("Tags")]
+        public List<String> Tags { get; set;}
+        */
+
+        //This one keeps the tag as the key and the count of how many times the tagis used in the value
+        [DisplayName("Tags")]
+        public Dictionary<String, int> TagAndCount { get; set; }
+
+        /*[DisplayName("Anonymous")]
+        public bool Anon { get; set; }*/
+    }
+}
