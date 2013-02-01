c397f06e55ab73db91752d332caed0f8560b9289
sec_goat <joshua.tilson@yahoo.com>
2013-02-01 07:16:55 -0500

    Working on feedback model and form, almost have it done just need to get the sending meail right

--- NextFlicksMVC4/Controllers/HomeController.cs
+++ NextFlicksMVC4/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Diagnostics;
 using NextFlicksMVC4.Filters;
+using NextFlicksMVC4.Models;
+using WebMatrix.WebData;
+using System.Web.Security;
+using System.Net.Mail;
 
 namespace NextFlicksMVC4.Controllers
 {
@@ -39,6 +43,37 @@ namespace NextFlicksMVC4.Controllers
 
         public ActionResult Feedback()
         {
+            MovieDbContext db = new MovieDbContext();
+            FeedbackModel feedback = new FeedbackModel();
+            
+
+            if (WebSecurity.IsAuthenticated)
+            {
+                int userID = WebSecurity.CurrentUserId;
+                var email = from user in db.Users
+                        where user.userID == userID
+                        select user.email;
+                feedback.Email = email.FirstOrDefault();
+            }
+            return View("Feedback", feedback);
+        }
+
+        [HttpPost]
+        public ActionResult Feedback(FeedbackModel feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                string from = feedback.Email;
+                string to = "joshua.tilson@yahoo.com";
+                string subject = "Feedback from WATW";
+                string body = feedback.Message;
+                MailMessage message = new MailMessage(from, to, subject, body);
+ 
+                SmtpClient client = new SmtpClient(host: "mail.wearethewatchers.com", port: 25);
+                client.Send(message);
+
+                //TODO: http://stackoverflow.com/questions/10022498/setting-up-email-settings-in-appsettings-web-config
+            }
             return View();
         }
     }
