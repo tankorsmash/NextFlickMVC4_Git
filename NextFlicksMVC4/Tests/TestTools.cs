cd51fde5c1cd4c461b1b0bcfff83697917cb21ce
TankorSmash <tankorsmash@gmail.com>
2013-03-07 17:07:17 -0500

    Added a test for IEnumToSelectListItem

--- NextFlicksMVC4/Tests/TestTools.cs
+++ NextFlicksMVC4/Tests/TestTools.cs
@@ -17,6 +17,24 @@ namespace NextFlicksMVC4.Tests
         [Test]
         public void TestIEnumToSelectListItem()
         {
+            List<SelectListItem> actual =
+                Tools.IEnumToSelectListItem(new int[] {1, 2, 3});
+
+            List<SelectListItem> expected = new List<SelectListItem>();
+            expected.Add(new SelectListItem {Text = "1", Value = "1"});
+            expected.Add(new SelectListItem {Text = "2", Value = "2"});
+            expected.Add(new SelectListItem {Text = "3", Value = "3"});
+
+            //This is bad, but unless I implement custom logic just for testing, this'll have to do
+            //    Could make it a forloop I guess...
+            Assert.AreEqual(expected[0].Text,actual[0].Text);
+            Assert.AreEqual(expected[0].Value,actual[0].Value);
+
+            Assert.AreEqual(expected[1].Text,actual[1].Text);
+            Assert.AreEqual(expected[1].Value,actual[1].Value);
+
+            Assert.AreEqual(expected[2].Text,actual[2].Text);
+            Assert.AreEqual(expected[2].Value,actual[2].Value);
         }
         
         [Test]
